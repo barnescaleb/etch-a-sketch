5d2a786b1902d45c1eaab0b6dbb871aaaf207f49
barnescaleb <barnescaleb119@gmail.com>
2022-08-22 12:58:23 -0700

    Replaced text input w/Slider and added CSS

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let color = 'black';
-
+let click = true;
 
 function expandBoard(size) {
     let board = document.querySelector('.board');
@@ -29,11 +29,13 @@ function changeSize(input) {
 }
 
 function colorSquare() {
-   if (color == 'random') {
-    this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
-   } else {
-    this.style.backgroundColor = color;
-   }
+    if (click) {
+        if (color == 'random') {
+            this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+        } else {
+            this.style.backgroundColor = color;
+        }
+    }
 }
 
 function changeColor(choice) {
@@ -45,3 +47,7 @@ function resetBoard() {
     let squares = board.querySelectorAll('div');
     squares.forEach((div) => div.style.backgroundColor = "white");
 }
+
+document.querySelector('body').addEventListener('click', () => {
+    click = !click;
+});
